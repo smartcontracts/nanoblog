efcbe0b3745e6de3acf98f93a72d45e96d344ff7
Kelvin Fichter <kelvin@optimism.io>
2023-05-24 12:32:32 -0400

    feat: add watching

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+const path = require('path')
 const { program } = require('commander')
+const server = require('live-server')
 const { version } = require('../package.json')
 const { compile } = require('../index.js')
 
@@ -15,6 +17,21 @@ program
   .option('-o, --output <output>', 'output directory', 'dist')
   .action((options) => {
     compile(options.input, options.output)
+  })
+
+program
+  .command('serve')
+  .description('serve your nanoblog locally for development')
+  .option('-i, --input <input>', 'input directory', 'src')
+  .option('-o, --output <output>', 'output directory', 'dist')
+  .option('-p, --port <port>', 'port to serve on', '8080')
+  .action((options) => {
+    server.start({ port: options.port, root: options.output, watch: [options.input] })
+    server.watcher.on('change', (e) => {
+      if (path.normalize(e).indexOf(path.normalize(options.output)) === -1) {
+        compile(options.input, options.output)
+      }
+    })
   })
 
 program.parse(process.argv)
