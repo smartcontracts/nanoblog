a82bcdd31ade9ba4d2f24b52594e5b2aca47b269
Kelvin Fichter <kelvinfichter@gmail.com>
2024-11-16 00:44:26 +0700

    feat: publish date

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const copyfiles = require('copyfiles')
 /**
  * Generates an HTML page with the given title and content.
  * @param {string} title Title for the page.
+ * @param {string} published Date the page was published.
  * @param {string} content Content for the page.
  * @returns {string} HTML page.
  */
-const template = (title, content) => {
+const template = (title, published, content) => {
   return `<!DOCTYPE html>
 <html>
   <head>
@@ -22,6 +23,7 @@ const template = (title, content) => {
   <body>
     <div class="nav"><a href="/">Home</a></div>
     <h1>${title}</h1>
+    <div><i>${published}</i></div>
     <div>
 ${content.replace(/\n$/, '')}
     </div>
@@ -38,7 +40,7 @@ const compile = async (inpdir, outdir) => {
     // Parse the input and turn it into HTML.
     const source = fs.readFileSync(input, 'utf8')
     const parsed = matter(source)
-    const html = template(parsed.data.title, marked.parse(parsed.content))
+    const html = template(parsed.data.title, parsed.data.published, marked.parse(parsed.content))
 
     // Write the output file.
     const output = input.replace(/\.md$/, '.html').replace(new RegExp(`^${inpdir}`), outdir)
